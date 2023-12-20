469e4a94dd5b3fc8c9c91d6ccc36fed693d18d6d
Iulia Puiu <iulia.puiu@hotmail.com>
2023-12-20 10:58:08 +0200

    Debug db

--- app/hooks/useDB.ts
+++ app/hooks/useDB.ts
@@ -1,6 +1,9 @@
 import * as SQLite from 'expo-sqlite';
 import { useEffect } from 'react';
 import { ResultSet } from 'expo-sqlite';
+import { Platform } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import * as Sharing from 'expo-sharing';
 
 export interface Product {
     id?: number;
@@ -17,6 +20,7 @@ export const useDB = () => {
 
     useEffect(() => {
         initDB();
+        //debugDB();
     }, []);
 
     const initDB = () => {
@@ -50,7 +54,7 @@ export const useDB = () => {
         const args = [name, price, description, image, quantity, category];
 
         return db.execAsync([{ sql, args }], false)
-         
+
     }
 
     const getAllCategories = () => {
@@ -81,13 +85,38 @@ export const useDB = () => {
         const args = [name, price, description, image, quantity, category, id];
 
         return db.execAsync([{ sql, args }], false)
-         
+
     }
 
     const deleteProduct = (id: number) => {
         const sql = `DELETE FROM products WHERE id = ?;`;
         const args = [id];
-        return db.execAsync([{sql, args}], false)
+        return db.execAsync([{ sql, args }], false)
+    }
+
+    const debugDB = async () => {
+        if (Platform.OS === 'android') {
+            const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync(FileSystem.documentDirectory);
+            if (permissions.granted) {
+                const base64 = await FileSystem.readAsStringAsync(FileSystem.documentDirectory + 'SQLite/shop.db',
+                    { encoding: FileSystem.EncodingType.Base64 });
+
+                await FileSystem.StorageAccessFramework.createFileAsync(permissions.directoryUri, 'shop.db', 'application/octet-stream')
+                    .then(async (uri) => {
+                        console.log("URI: ", uri);
+
+                        await FileSystem.writeAsStringAsync(uri, base64, { encoding: FileSystem.EncodingType.Base64 });
+                    })
+                    .catch((error) => {
+                        console.log("Error: ", error);
+                    })
+            } else {
+                console.log("No permissions granted");
+            }
+        } else {
+            console.log(FileSystem.documentDirectory);
+            await Sharing.shareAsync(FileSystem.documentDirectory + 'SQLite/shop.db');
+        }
     }
 
 
